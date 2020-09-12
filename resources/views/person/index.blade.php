4b9f7b4004c897e562094858aabe9be4f5c9f80a
medumi <medumi@icloud.com>
2020-09-12 10:15:26 +0300

    small fixes

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -14,22 +14,22 @@
                             </form>
                         </div>
                         <div class="d-inline-block" style="vertical-align: top; padding: 6px;">
-                            <h4>{{ __('People') }}</h4>
+                            <h4 >{{ __('People') }}</h4>
                         </div>
                         <find route="person" fields="name,email,phone,address" search="{{ $search ?? '' }}"></find>
                     </div>
                     <div class="card-body">
-                        <table class="table table-sm table-striped">
+                        <table class="table table-sm table-striped table-responsive-md">
                             <thead class="thead">
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">{{ __("Name") }}</th>
-                                <th scope="col">{{ __("Email") }}</th>
-                                <th scope="col">{{ __("Phone") }}</th>
-                                <th scope="col">{{ __("Company Id") }}</th>
-                                <th scope="col">{{ __("Address") }}</th>
+								<th scope="col">{{ __("Email") }}</th>
+								<th scope="col">{{ __("Phone") }}</th>
+								<th scope="col">{{ __("Company") }}</th>
+								<th scope="col">{{ __("Address") }}</th>
 
-                                <th scope="col">{{ __('Actions') }}</th>
+                                <th scope="col" style="width: 100px;">{{ __('Actions') }}</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -37,22 +37,20 @@
                                 <tr>
                                     <th scope="row">{{ $person->id  }}</th>
                                     <td>{{ $person->name }}</td>
-                                    <td>{{ $person->email }}</td>
-                                    <td>{{ $person->phone }}</td>
-                                    <td>{{ $person->company->name }}</td>
-                                    <td>{{ $person->address }}</td>
+									<td>{{ $person->email }}</td>
+									<td>{{ $person->phone }}</td>
+									<td>{{ $person->company->name }}</td>
+									<td>{{ $person->address }}</td>
 
                                     <td>
                                         <form class="float-right" action="{{ route('person.destroy', $person->id) }}"
                                               method="post" onsubmit="return confirm('Do you really want to delete?');">
                                             @method('delete')
                                             @csrf
-                                            <button class="btn btn-sm btn-outline-danger" type="submit"><i
-                                                    class="fas fa-trash"></i></button>
+                                            <button class="btn btn-sm btn-outline-danger" type="submit"><i class="fas fa-trash"></i></button>
                                         </form>
                                         <a class="btn btn-sm btn-outline-success float-right" style="margin: 0 8px;"
-                                           href="{{ route('person.edit', $person->id) }}"><i
-                                                class="fas fa-edit"></i></a>
+                                           href="{{ route('person.edit', $person->id) }}"><i class="fas fa-edit"></i></a>
                                     </td>
                                 </tr>
                             @endforeach
